bbfbf14f910fe6f1fe0bc2792d441bd6fa4f0754
ttheine <82824733+ttheine@users.noreply.github.com>
2022-01-09 20:46:24 +0100

    Delete src/de/uni/koeln/se directory

--- src/de/uni/koeln/se/Main.java
+++
@@ -1,8 +0,0 @@
-package de.uni.koeln.se;
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!\n");
-    System.out.println(":) \n");
-    System.out.println("Goodbye World!");
-  }
-}
