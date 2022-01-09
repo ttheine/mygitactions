a7b610b5aadf17e04c83849f1605c3f4fb1bae45
ttheine <82824733+ttheine@users.noreply.github.com>
2022-01-09 20:39:29 +0100

    Create Main.java

---
+++ src/de/uni/koeln/se/Main.java
@@ -0,0 +1,8 @@
+package de.uni.koeln.se;
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!\n");
+    System.out.println(":) \n");
+    System.out.println("Goodbye World!");
+  }
+}
